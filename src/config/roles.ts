d102181068e7d3bd4a07f51d196c0d7340bdc248
Opal Aja <opank2441@gmail.com>
2025-06-11 15:03:50 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/config/roles.ts
+++ src/config/roles.ts
@@ -1,7 +1,10 @@
+
 export const ROLES = ["admin", "guru", "siswa", "orangtua"] as const;
 export type Role = (typeof ROLES)[number];
 
 export const roleDisplayNames: Record<Role, string> = {
   admin: "Admin",
   guru: "Guru",
-  
+  siswa: "Siswa",
+  orangtua: "Orang Tua",
+};
