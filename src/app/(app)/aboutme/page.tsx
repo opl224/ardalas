c92fd1b36896a998dc102d59fa98ba09c39a319e
Opal Aja <opank2441@gmail.com>
2025-06-24 16:39:30 +0000

    masih berada di tengah buat berada di pojok kanan atas

--- src/app/(app)/aboutme/page.tsx
+++ src/app/(app)/aboutme/page.tsx
@@ -37,7 +37,7 @@ export default function AboutMePage() {
               enableTilt={true}
               onContactClick={() => window.location.href = 'mailto:nnovalfirdaus@gmail.com'}
           />
-          <div className="absolute top-[35px] right-[55px] z-20 opacity-50 scale-75">
+          <div className="absolute top-[-45px] right-[-45px] z-20 opacity-50 scale-75">
              <CircularText
                text="NOVAL-FIRDAUS-NOVAL-FIRDAUS-"
                onHover="speedUp"
